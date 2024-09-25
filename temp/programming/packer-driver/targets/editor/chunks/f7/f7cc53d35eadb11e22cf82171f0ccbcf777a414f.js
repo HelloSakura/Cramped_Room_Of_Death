e3fb36ba0cbeedb90647e48a679c8a2cbcae6b8a
duanhao303 <blight98@163.com>
2024-09-25 23:04:02 +0800

    初始化工程

---
+++ temp/programming/packer-driver/targets/editor/chunks/f7/f7cc53d35eadb11e22cf82171f0ccbcf777a414f.js
@@ -0,0 +1,83 @@
+System.register(["__unresolved_0", "cc", "__unresolved_1", "__unresolved_2", "__unresolved_3"], function (_export, _context) {
+  "use strict";
+
+  var _reporterNs, _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, AnimationClip, State, DIRECTION_ENUM, DirectionSubStateMachine, IdleSubStateMachine, _crd, BASE_URL;
+
+  function _reportPossibleCrUseOfState(extras) {
+    _reporterNs.report("State", "../../base/State", _context.meta, extras);
+  }
+
+  function _reportPossibleCrUseOfStateMachine(extras) {
+    _reporterNs.report("StateMachine", "../../base/StateMachine", _context.meta, extras);
+  }
+
+  function _reportPossibleCrUseOfDIRECTION_ENUM(extras) {
+    _reporterNs.report("DIRECTION_ENUM", "../../enums", _context.meta, extras);
+  }
+
+  function _reportPossibleCrUseOfDirectionSubStateMachine(extras) {
+    _reporterNs.report("DirectionSubStateMachine", "../../base/DirectionSubStateMachine", _context.meta, extras);
+  }
+
+  _export("default", void 0);
+
+  return {
+    setters: [function (_unresolved_) {
+      _reporterNs = _unresolved_;
+    }, function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      AnimationClip = _cc.AnimationClip;
+    }, function (_unresolved_2) {
+      State = _unresolved_2.default;
+    }, function (_unresolved_3) {
+      DIRECTION_ENUM = _unresolved_3.DIRECTION_ENUM;
+    }, function (_unresolved_4) {
+      DirectionSubStateMachine = _unresolved_4.default;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "5d9a4/aXgdCNLgUViK4WpKA", "IdleSubStateMachine", undefined);
+
+      __checkObsolete__(['AnimationClip']);
+
+      BASE_URL = "texture/woodenskeleton/idle";
+
+      _export("default", IdleSubStateMachine = class IdleSubStateMachine extends (_crd && DirectionSubStateMachine === void 0 ? (_reportPossibleCrUseOfDirectionSubStateMachine({
+        error: Error()
+      }), DirectionSubStateMachine) : DirectionSubStateMachine) {
+        constructor(fsm) {
+          super(fsm);
+          this.stateMachine.set((_crd && DIRECTION_ENUM === void 0 ? (_reportPossibleCrUseOfDIRECTION_ENUM({
+            error: Error()
+          }), DIRECTION_ENUM) : DIRECTION_ENUM).TOP, new (_crd && State === void 0 ? (_reportPossibleCrUseOfState({
+            error: Error()
+          }), State) : State)(fsm, `${BASE_URL}/top`, AnimationClip.WrapMode.Loop));
+          this.stateMachine.set((_crd && DIRECTION_ENUM === void 0 ? (_reportPossibleCrUseOfDIRECTION_ENUM({
+            error: Error()
+          }), DIRECTION_ENUM) : DIRECTION_ENUM).BOTTOM, new (_crd && State === void 0 ? (_reportPossibleCrUseOfState({
+            error: Error()
+          }), State) : State)(fsm, `${BASE_URL}/bottom`, AnimationClip.WrapMode.Loop));
+          this.stateMachine.set((_crd && DIRECTION_ENUM === void 0 ? (_reportPossibleCrUseOfDIRECTION_ENUM({
+            error: Error()
+          }), DIRECTION_ENUM) : DIRECTION_ENUM).LEFT, new (_crd && State === void 0 ? (_reportPossibleCrUseOfState({
+            error: Error()
+          }), State) : State)(fsm, `${BASE_URL}/left`, AnimationClip.WrapMode.Loop));
+          this.stateMachine.set((_crd && DIRECTION_ENUM === void 0 ? (_reportPossibleCrUseOfDIRECTION_ENUM({
+            error: Error()
+          }), DIRECTION_ENUM) : DIRECTION_ENUM).RIGHT, new (_crd && State === void 0 ? (_reportPossibleCrUseOfState({
+            error: Error()
+          }), State) : State)(fsm, `${BASE_URL}/right`, AnimationClip.WrapMode.Loop));
+        }
+
+      });
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=f7cc53d35eadb11e22cf82171f0ccbcf777a414f.js.map
